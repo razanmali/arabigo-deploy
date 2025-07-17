290249c92be6b7777d9e0577d45c1a86da2de20e
Razan Ali <107950889+razanmali@users.noreply.github.com>
2025-07-17 15:25:08 +0400

    Added sidebar

--- arabigo/app/(marketing)/footer.tsx
+++ arabigo/app/(marketing)/footer.tsx
@@ -1,9 +1,23 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
 export const Footer = () =>{
     return(
 
         <footer className="hidden lg:block h-20 w-full
         border-t-2 border-slate-200 p-2">
-            Footer
+            <div className="max-w-screen-lg mx-auto flex
+            items-center justify-evenly h-full">
+                <Button size="lg" variant="ghost" className="w-full">
+                    <Image src="ar.svg" 
+                    alt="Arabic" 
+                    height={32} 
+                    width={40}
+                    className="mr-4 rounded-md"/>
+                    Arabic
+                </Button>
+
+            </div>
         </footer>
     );
 };

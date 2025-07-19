d09d96e12502c6c381eba0e7d6a0b894c16cc723
Razan Ali <107950889+razanmali@users.noreply.github.com>
2025-07-20 01:50:10 +0400

    added learnPage header

--- arabigo/app/(main)/learn/page.tsx
+++ arabigo/app/(main)/learn/page.tsx
@@ -1,7 +1,22 @@
+import { StickyWrapper } from "@/components/sticky-wrapper";
+import { UserProgress } from "@/components/user-progress";
+import { FeedWrapper } from "@/components/feed-wrapper";
+import { Header } from "./header";
 const LearnPage = () =>{
     return(
-        <div>
-            Learn Page
+        <div className="flex flex-row-reverse gap-[48px] px-6">
+           <StickyWrapper>
+           <UserProgress
+           activeCourse={{title:"Arabic", imageSrc:"/ar.svg"}}
+           hearts={5}
+           points={100}
+           hasActiveSubscription={false}>
+
+           </UserProgress>
+           </StickyWrapper>
+           <FeedWrapper>
+           <Header title="Arabic"/>
+           </FeedWrapper>
         </div>
     )
 }
